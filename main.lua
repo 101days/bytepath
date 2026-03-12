f745a52962914b10d4011413eefa52d089b2e308
mancuoj <mancuoj@gmail.com>
2026-03-13 00:03:36 +0800

    accumulator fixed dt

--- main.lua
+++ main.lua
@@ -9,3 +9,42 @@ end
 function love.draw()
 
 end
+
+function love.run()
+    if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+    if love.timer then love.timer.step() end
+
+    local dt = 0
+    local fixed_dt = 1 / 60
+    local accumulator = 0
+
+    return function()
+        if love.event then
+            love.event.pump()
+            for name, a, b, c, d, e, f in love.event.poll() do
+                if name == "quit" then
+                    if not love.quit or not love.quit() then
+                        return a or 0
+                    end
+                end
+                love.handlers[name](a, b, c, d, e, f)
+            end
+        end
+
+        if love.timer then dt = love.timer.step() end
+        accumulator = accumulator + dt
+        while accumulator >= fixed_dt do
+            if love.update then love.update(fixed_dt) end
+            accumulator = accumulator - fixed_dt
+        end
+
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.origin()
+            love.graphics.clear(love.graphics.getBackgroundColor())
+            if love.draw then love.draw() end
+            love.graphics.present()
+        end
+
+        if love.timer then love.timer.sleep(0.001) end
+    end
+end

3b73bf8fa5465a009f4cb56fa6fe2c9ff53e410e
mancuoj <mancuoj@gmail.com>
2026-03-16 02:07:11 +0800

    import moses

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 object = require "libs/classic/classic"
 input_handler = require "libs/input/Input"
 enhanced_timer = require "libs/enhanced_timer/enhanced_timer"
+fn = require 'libs/moses/moses'
 
 function love.load()
     local objects_files = {}
@@ -9,28 +10,6 @@ function love.load()
 
     input = input_handler()
     timer = enhanced_timer()
-
-    hp_bar_bg = { x = 300, y = 100, w = 200, h = 40 }
-    hp_bar_fg = { x = 300, y = 100, w = 200, h = 40 }
-
-    input:bind('d', function()
-        local target_fg = math.max(0, hp_bar_fg.w - 25)
-        timer:tween('fg', 0.5, hp_bar_fg, { w = target_fg }, 'in-out-cubic')
-        timer:after('bg_after', 0.25, function()
-            local target_bg = math.max(0, hp_bar_bg.w - 25)
-            timer:tween('bg', 0.5, hp_bar_bg, { w = target_bg }, 'in-out-cubic')
-        end)
-    end)
-
-    circle = { radius = 24 }
-    input:bind('e', function()
-        timer:cancel('shrink')
-        timer:tween('expand', 6, circle, { radius = 96 }, 'in-out-cubic')
-    end)
-    input:bind('s', function()
-        timer:cancel('expand')
-        timer:tween('shrink', 6, circle, { radius = 24 }, 'in-out-cubic')
-    end)
 end
 
 function love.update(dt)
@@ -38,21 +17,6 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(222 / 255, 64 / 255, 64 / 255)
-    love.graphics.rectangle(
-        'fill',
-        hp_bar_bg.x, hp_bar_bg.y,
-        hp_bar_bg.w, hp_bar_bg.h
-    )
-    love.graphics.setColor(222 / 255, 96 / 255, 96 / 255)
-    love.graphics.rectangle(
-        'fill',
-        hp_bar_fg.x, hp_bar_fg.y,
-        hp_bar_fg.w, hp_bar_fg.h
-    )
-    love.graphics.setColor(1, 1, 1)
-
-    love.graphics.circle('fill', 400, 300, circle.radius)
 end
 
 function require_files(files)

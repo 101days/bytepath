86487d16c4cb6496586335f090a0480c7160f0b7
mancuoj <mancuoj@gmail.com>
2026-03-21 11:39:42 +0800

    restart

--- main.lua
+++ main.lua
@@ -1,69 +1,12 @@
-object = require "libs/classic/classic"
-input_handler = require "libs/input/Input"
-timer_handler = require "libs/enhanced_timer/enhanced_timer"
-camera_handler = require 'libs/hump/camera'
-fn = require 'libs/moses/moses'
-physics = require 'libs/windfield/windfield'
-
-require "util"
-require "game_object"
-require "area"
-
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.graphics.setLineStyle('rough')
-
-    local object_files = {}
-    recursive_enumerate("objects", object_files)
-    require_files(object_files)
-
-    local room_files = {}
-    recursive_enumerate("rooms", room_files)
-    require_files(room_files)
-
-    input = input_handler()
-    timer = timer_handler()
-    camera = camera_handler()
-
-    input:bind('left', 'left')
-    input:bind('right', 'right')
-
-    current_room = nil
-    goto_room('stage')
-
-    resize(2)
 end
 
 function love.update(dt)
-    timer:update(dt)
-    camera:update(dt)
-    if current_room then current_room:update(dt) end
 end
 
 function love.draw()
-    if current_room then current_room:draw() end
-end
-
-function goto_room(room_type, ...)
-    current_room = _G[room_type](...)
-end
-
-function require_files(files)
-    for _, file in ipairs(files) do
-        require(file:sub(1, -5))
-    end
-end
-
-function recursive_enumerate(folder, file_list)
-    for _, item in ipairs(love.filesystem.getDirectoryItems(folder)) do
-        local path = folder .. "/" .. item
-        local info = love.filesystem.getInfo(path)
-        if info.type == "file" then
-            table.insert(file_list, path)
-        elseif info.type == "directory" then
-            recursive_enumerate(path, file_list)
-        end
-    end
 end
 
 function love.run()
